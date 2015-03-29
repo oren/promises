5798a7603bd8d14446940defc07903f95f18953d
Oren <orengolan@gmail.com>
2015-03-29 10:56:35 -0700

    test

---
+++ promises.js
@@ -0,0 +1,27 @@
+// login.then(getContacts).then(getAttachments);
+
+function getDonors(req, res, next) {
+  var login = new Promise(loginPromise);
+
+  function loginPromise(resolve, reject) {
+    resolve(true);
+  }
+
+  var getContacts = new Promise(getContactsPromise);
+
+  function getContactsPromise(resolve, reject) {
+    resolve({contacts: [{name: 'josh'}, {name: 'lena'}]});
+  }
+
+  var getAttachments = new Promise(getAttachments);
+
+  function getAttachments(resolve, reject) {
+    console.log('done');
+    results[0].attachments = [{id: 1}, {id: 42}];
+    results[1].attachments = [{id: 3}, {id: 45}];
+  }
+
+  login.then(getContacts).then(getAttachments);
+}
+
+getDonors();
