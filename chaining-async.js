08eb9ded60cde2150fcc9b730e6f12889056a6e8
Oren <orengolan@gmail.com>
2015-03-29 18:15:49 -0700

    chaining

--- chaining-async.js
+++ chaining-async.js
@@ -1,22 +1,46 @@
 // 2. chaining promises. everything is async
 
-var login = new Promise(loginPromise);
+// var login = new Promise(function(resolve, reject) {
+//     setTimeout(function() {
+//       resolve('logged in.');
+//     }, 1000);
+// })
 
-function loginPromise(resolve, reject) {
-  // do stuff. probably async
-  resolve('logged in.');
+function login(result) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      resolve('logged in.');
+    }, 1000);
+  })
 }
 
-var getContacts = new Promise(getContactsPromise);
+function getContacts(result) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      resolve(result + ' get contacts.');
+    }, 1000);
+  })
+}
 
-function getContactsPromise(resolve, reject) {
-  setTimeout(function() {
-    resolve(' get contacts.');
-  }, 2000);
+function someSync(result) {
+  return result + ' do something sync.';
+}
+
+function someAsync(result) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      resolve(result + ' some async.');
+    }, 1000);
+  })
 }
 
 function getAttachments(result) {
   console.log(result + ' get attachments');
 }
 
-login.then(getContacts).then(getAttachments);
+login().then(getContacts).then(someSync).then(someAsync).then(getAttachments);
+// => logged in. get contacts. do something sync. some async. get attachments
+
+// There are 2 types of functions that can be used: 
+// 1. simple function that accepts and returns a single value.  
+// 2. function that accepts a single value and returns a promise.
